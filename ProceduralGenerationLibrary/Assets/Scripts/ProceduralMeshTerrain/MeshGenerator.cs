c1d778fae00d909918803284b3049672eb5838c9
Mark <mdargie97@gmail.com>
2021-04-19 17:56:44 +0100

    Initial Setup for Terrain Mesh Generation Scene

---
+++ ProceduralGenerationLibrary/Assets/Scripts/ProceduralMeshTerrain/MeshGenerator.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGenerator : MonoBehaviour
+{
+
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+    public int xSize = 20;
+    public int zSize = 20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
+    }
+
+
+    void CreateShape()
+    {
+
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= zSize; x++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+                i++;
+            }
+        }
+
+        triangles = new int[6];
+        triangles[0] = 0;
+        triangles[1] = xSize + 1;
+        triangles[2] = 1;
+        triangles[3] = 1;
+        triangles[4] = xSize + 1;
+        triangles[5] = xSize + 2;
+
+        //vertices = new Vector3[]
+        //{
+        //    new Vector3(0,0,0),
+        //    new Vector3(0,0,1),
+        //    new Vector3(1,0,0),
+        //    new Vector3(1,0,1)
+        //};
+
+        //triangles = new int[]
+        //{
+        //    0, 1, 2,
+        //    1, 3, 2
+        //};
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+
+    }
+
+    private void OnDrawGizmos()
+    {
+
+        if (vertices == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], .1f);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
